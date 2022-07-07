12446e3d7262ee06002ea79ff4fc3347b91e6030
Leandri Beavis <leandrikuyk@gmail.com>
2022-07-07 19:10:30 -0400

    added commentHandler function with event listener

--- public/javascript/comment.js
+++ public/javascript/comment.js
@@ -1,10 +1,28 @@
 async function commentHandler(e) {
     e.preventDefault();
 
-    // TODO: Create Response 
-    // await fetch ?
-    // POST method ?
+    const comment_text = $('textarea[name="comment-body"]').value.trim();
+    const post_id = window.location.toString().split('/') [
+        window.location.toString().split('/').length - 1];
     
+    if (comment_text) {
+        const res = await fetch ('/api/comments', {
+            method: 'POST',
+            body: JSON.stringify({
+                comment_text,
+                post_id
+            }),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+        if (res.ok) {
+            document.location.reload();
+        } else {
+            alert(res.statusText)
+        }
+    }
 }
 
-// TODO: addEventListener for commentHandler
+
+$('.comment-form').addEventListener('submit', commentHandler);
