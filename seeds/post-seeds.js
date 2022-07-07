93286251ba3a3c9bdaa89d2d5dee075993390a1a
BenHuf <benthehuffman@gmail.com>
2022-07-07 12:48:49 -0400

    Added spotify links to a few post seeds

--- seeds/post-seeds.js
+++ seeds/post-seeds.js
@@ -3,32 +3,32 @@ const { Post } = require('../models');
 const postdata = [
     {
         title: 'Donec posuere metus vitae ipsum.',
-        post_url: 'https://buzzfeed.com/in/imperdiet/et/commodo/vulputate.png',
+        post_url: 'https://open.spotify.com/track/4a5a4NpPcsp8lKLjsGqks2?si=38a65e07b5294955',
         user_id: 10
     },
     {
         title: 'Morbi non quam nec dui luctus rutrum.',
-        post_url: 'https://nasa.gov/donec.json',
+        post_url: 'https://open.spotify.com/track/2qtryeKSrI7RgSBo1N3uXv?si=1ba17f30705f4bcf',
         user_id: 8
     },
     {
         title: 'Donec diam neque, vestibulum eget, vulputate ut, ultrices vel, augue.',
-        post_url: 'https://europa.eu/parturient/montes/nascetur/ridiculus/mus/etiam/vel.aspx',
+        post_url: 'https://open.spotify.com/track/0bjk1vn5sMOoZOPqSzwoTC?si=2cf42aa9223d4fe1',
         user_id: 1
     },
     {
         title: 'Nunc purus.',
-        post_url: 'http://desdev.cn/enim/blandit/mi.jpg',
+        post_url: 'https://open.spotify.com/track/69OR7bYhVphLuTd9dyJjT9?si=d3a6fec649214f2c',
         user_id: 4
     },
     {
         title: 'Pellentesque eget nunc.',
-        post_url: 'http://google.ca/nam/nulla/integer.aspx',
+        post_url: 'https://open.spotify.com/track/2IKix80lBA5ppm6096p5U7?si=da9f7b15e3f54ed1',
         user_id: 7
     },
     {
         title: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit.',
-        post_url: 'https://stanford.edu/consequat.png',
+        post_url: 'https://open.spotify.com/track/0UV1xzcuNfWs4fWc8ZsB3Y?si=59eb9c9e19144868',
         user_id: 4
     },
     {
