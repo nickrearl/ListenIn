c6e1493bda80c29183c81b9c1ede83fc4e263e28
Nick Earl <nick.r.earl@gmail.com>
2022-07-07 21:36:11 -0400

    partials print post data on homepage

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,11 +1,45 @@
 const router = require('express').Router();
 const {Comment, Post, User, Vote } = require('../models')
+const sequelize = require('../config/connection')
 
-router.get('/', (req, res) => {
+// router.get('/', (req, res) => {
+//     res.render('homepage', {
+//     });
+// })
 
-    res.render('homepage', {
-    });
-})
+router.get('/', async (req, res) => {
+    try {
+        const dbPostData = await Post.findAll({
+            attributes: [
+                'id',
+                'post_url',
+                'title',
+                'created_at',
+                [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'),
+                'vote_count'
+                ],
+                'iframe'
+            ],
+            include: [
+                {
+                    model: User,
+                    attributes: ['username']
+                }
+            ]
+        });
+
+        const posts = dbPostData.map((post) =>
+            post.get({ plain: true })
+        );
+
+        res.render('homepage', {
+            posts,
+        });
+    } catch (err) {
+        console.log(err);
+        res.status(500).json(err);
+    }
+});
 
 router.get('/login', (req, res) => {
 
