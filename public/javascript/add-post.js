0e0ba62503e5f70b1c4ae00930b4b0f50b181c27
BenHuf <benthehuffman@gmail.com>
2022-07-09 17:39:04 -0400

    Seed/New Post style changes

--- public/javascript/add-post.js
+++ public/javascript/add-post.js
@@ -8,7 +8,7 @@ async function parsing(post_url) {
     let title = data.title;
     console.log(data);
     let thumbnail = data.thumbnail_url;
-    let iframe = '<iframe src="https://open.spotify.com/embed/track/' + blob + '?utm_source=generator" width="300" height="300" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"></iframe>'
+    let iframe = '<iframe src="https://open.spotify.com/embed/track/' + blob + '?utm_source=generator" width="100%" height="300" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"></iframe>'
 // <iframe style="border-radius:12px" src="https://open.spotify.com/embed/track/3aIhJDHxr1kgTSnutJxPTH?utm_source=generator" width="100%" height="380" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"></iframe>
     return { title, thumbnail, iframe};
 }
--- server.js
+++ server.js
@@ -11,7 +11,9 @@ const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const sess = {
     secret: 'Super secret secret',
-    cookie: {},
+    cookie: {
+        sameSite: 'none'
+    },
     resave: false,
     saveUninitialized: true,
     store: new SequelizeStore({
